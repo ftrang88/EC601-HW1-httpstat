6525b264fdf9771f940793bd92b28659cfb9a266
Frank <ftrang88@gmail.com>
2017-09-09 13:17:11 -0400

    Color change
    Changed text that was green --> magenta
    Changed text that was cyan ---> red

--- httpstat.py
+++ httpstat.py
@@ -259,7 +259,7 @@ def main():
     # ip
     if show_ip:
         s = 'Connected to {}:{} from {}:{}'.format(
-            cyan(d['remote_ip']), cyan(d['remote_port']),
+            red(d['remote_ip']), red(d['remote_port']),
             d['local_ip'], d['local_port'],
         )
         print(s)
@@ -275,10 +275,10 @@ def main():
     for loop, line in enumerate(headers.split('\n')):
         if loop == 0:
             p1, p2 = tuple(line.split('/'))
-            print(green(p1) + grayscale[14]('/') + cyan(p2))
+            print(magenta(p1) + grayscale[14]('/') + red(p2))
         else:
             pos = line.find(':')
-            print(grayscale[14](line[:pos + 1]) + cyan(line[pos + 1:]))
+            print(grayscale[14](line[:pos + 1]) + red(line[pos + 1:]))
 
     print()
 
@@ -300,7 +300,7 @@ def main():
             print(body)
     else:
         if save_body:
-            print('{} stored in: {}'.format(green('Body'), bodyf.name))
+            print('{} stored in: {}'.format(magenta('Body'), bodyf.name))
 
     # remove body file
     if not save_body:
@@ -319,10 +319,10 @@ def main():
     template = '\n'.join(tpl_parts)
 
     def fmta(s):
-        return cyan('{:^7}'.format(str(s) + 'ms'))
+        return red('{:^7}'.format(str(s) + 'ms'))
 
     def fmtb(s):
-        return cyan('{:<7}'.format(str(s) + 'ms'))
+        return red('{:<7}'.format(str(s) + 'ms'))
 
     stat = template.format(
         # a
