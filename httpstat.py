24f87236c58a1d0bf2ecfa148f62981732a7e448
Jason_ZisenZhou <zzsen82@163.com>
2017-09-11 22:30:32 -0400

    Update httpstat.py

--- httpstat.py
+++ httpstat.py
@@ -97,13 +97,13 @@ def make_color(code):
     return color_func
 
 
-red = make_color(30)
+red = make_color(32)
 green = make_color(31)
-yellow = make_color(32)
+yellow = make_color(34)
 blue = make_color(33)
-magenta = make_color(34)
+magenta = make_color(36)
 cyan = make_color(35)
-white = make_color(36)
+white = make_color(37)
 bold = make_color(1)
 underline = make_color(4)
 
