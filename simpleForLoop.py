382b1bd2e67b91e60d4d299fb7500e1b95789518
Frank <ftrang88@gmail.com>
2017-09-13 18:16:56 -0400

    New file with for loop
    yay for loops

---
+++ simpleForLoop.py
@@ -0,0 +1,12 @@
+# Frank Tranghese
+# Boston University College of Engineering
+# EC601 - HW 1 - Group 14
+# 13 Sept 2016
+
+a = 5
+b = 10
+c = 0
+
+for x in range(1,5):
+	c += a+b
+	print(x,'(',a,'+',b,') =',c)
